cba2799f4037e2afc4fec845a331104dd450d365
Moutazbaaj <moutazbaaj@gmail.com>
2024-06-01 14:41:51 +0200

    Initial Commit

---
+++ Car gallery/Car_galleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_galleryApp.swift
+//  Car gallery
+//
+//  Created by Moutaz Baaj on 01.06.24.
+//
+
+import SwiftUI
+
+@main
+struct Car_galleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
